6c6b13a697141bed246cca0818e9f5fe4c3b3684
Jorge Moreno <jsmorenop@unal.edu.co>
2021-09-13 15:32:35 -0500

    Optimizar importaciones en node

---
+++ .history/middlewares/index_20210913152613.js
@@ -0,0 +1,3 @@
+const validateFields = require("../middlewares/validate-fields");
+const validateJWT = require("../middlewares/validate-jwt");
+const validateRoles = require("../middlewares/validate-roles");
